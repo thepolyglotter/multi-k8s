0e4940512ea3fa1e88f2667ceddf0f4ff356aa49
Kanwar, Nitin <kanwarnitin@gmail.com>
2019-05-16 12:57:16 +0100

    renamed tag kanwarnitin to nkanwar

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t kanwarnitin/multi-client:latest -t kanwarnitin/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t kanwarnitin/multi-server:latest -t kanwarnitin/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t kanwarnitin/multi-worker:latest -t kanwarnitin/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t nkanwar/multi-client:latest -t nkanwar/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t nkanwar/multi-server:latest -t nkanwar/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t nkanwar/multi-worker:latest -t nkanwar/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push kanwarnitin/multi-client:latest
-docker push kanwarnitin/multi-server:latest
-docker push kanwarnitin/multi-worker:latest
+docker push nkanwar/multi-client:latest
+docker push nkanwar/multi-server:latest
+docker push nkanwar/multi-worker:latest
 
-docker push kanwarnitin/multi-client:$SHA
-docker push kanwarnitin/multi-server:$SHA
-docker push kanwarnitin/multi-worker:$SHA
+docker push nkanwar/multi-client:$SHA
+docker push nkanwar/multi-server:$SHA
+docker push nkanwar/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=kanwarnitin/multi-server:$SHA
-kubectl set image deployments/client-deployment client=kanwarnitin/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=kanwarnitin/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=nkanwar/multi-server:$SHA
+kubectl set image deployments/client-deployment client=nkanwar/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=nkanwar/multi-worker:$SHA
